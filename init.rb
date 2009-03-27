c852cd80fa281be0bc34b311f5f265ee185467bf
cveneziani <cveneziani@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-03-27 11:50:25 +0000

    Bad init file

--- init.rb
+++ init.rb
@@ -10,8 +10,7 @@ if File.exists?(locale_path)
   locale_files = Dir[File.join(locale_path, '*.{rb,yml}')]
   unless locale_files.blank?
     first_app_element = 
-      I18n.load_path.select{ |e| e =~ /^#{ RAILS_ROOT }/ }.reject{ |e| e 
-=~ /^#{ RAILS_ROOT }\/vendor\/plugins/ }.first
+      I18n.load_path.select{ |e| e =~ /^#{ RAILS_ROOT }/ }.reject{ |e| e =~ /^#{ RAILS_ROOT }\/vendor\/plugins/ }.first
     app_index = I18n.load_path.index(first_app_element) || - 1
     I18n.load_path.insert(app_index, *locale_files)
   end

0dbc1a73caf6be07dacd2887cac5b4f9bd7fe442
cveneziani <cveneziani@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-03-30 14:43:35 +0000

    Page content can be blank

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ActiveRecord::Base
 
   validates_presence_of     :title
-  validates_presence_of     :content
+#  validates_presence_of     :content
   validates_presence_of     :url
 
   validates_uniqueness_of   :single_key, :if => Proc.new {|c| c.single_key}

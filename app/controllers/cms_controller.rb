17f8f72a2a6884d82e4ee360126610ab0cd55cbe
cveneziani <cveneziani@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-04-14 12:57:17 +0000

    Made section variable available in cms show view

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -1,17 +1,17 @@
 class CmsController < ApplicationController
   def show
     # find page by url and section params
-    section = Section::find_sub_section params[:sections]
-    @page = Page.find_by_url_and_active params[:url], true, :conditions => get_conditions(section)
+    @section = Section::find_sub_section params[:sections]
+    @page = Page.find_by_url_and_active params[:url], true, :conditions => get_conditions(@section)
 
     # if page is not found, find section by url param and then page
     if !@page
-      unless section
-        section = Section.find_by_url params[:url]
+      unless @section
+        @section = Section.find_by_url params[:url]
       else
-        section = section.children.find_by_url params[:url]
+        @section = @section.children.find_by_url params[:url]
       end
-      @page = Page.find_by_url_and_active params[:url], true, :conditions => get_conditions(section)
+      @page = Page.find_by_url_and_active params[:url], true, :conditions => get_conditions(@section)
     end
 
     @blocks = @page.blocks if @page

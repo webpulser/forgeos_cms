33296387a5e64c2f54d22b75ce1fe24eea06c4b1
clepagnot <clepagnot@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-07-27 09:42:11 +0000

    - merge with trunk from r113 to r124

---
+++ lib/rails_content.rb
@@ -0,0 +1,7 @@
+# Set administration's menu
+Forgeos::AdminMenu << { :title => 'sections', :url => { :controller => 'admin/sections' }, :i18n => true }
+Forgeos::AdminMenu << { :title => 'pages', :url => { :controller => 'admin/pages' }, :i18n => true,
+  :children => [
+    { :title => 'blocks', :url => { :controller => 'admin/blocks' }, :i18n => true }
+  ]
+}

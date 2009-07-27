411d78f359c35e2f079c738c2a8c2e2b71bf1baa
clepagnot <clepagnot@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-07-27 13:21:37 +0000

    - remove common locales

--- init.rb
+++ init.rb
@@ -1,16 +1,2 @@
-# require_plugin 'habtm_list'
-# require_plugin 'restful_authentication'
-# require_plugin 'attachment_fu'
-
-locale_path = File.join(directory, 'config/locales')
-if File.exists?(locale_path)
-  locale_files = Dir[File.join(locale_path, '*.{rb,yml}')]
-  unless locale_files.blank?
-    first_app_element = 
-      I18n.load_path.select{ |e| e =~ /^#{ RAILS_ROOT }/ }.reject{ |e| e =~ /^#{ RAILS_ROOT }\/vendor\/plugins/ }.first
-    app_index = I18n.load_path.index(first_app_element) || - 1
-    I18n.load_path.insert(app_index, *locale_files)
-  end
-end
-
+I18n.load_path += Dir[Rails.root.join('vendor', 'plugins', 'rails_content', 'config', 'locales', '*.{rb,yml}')]
 require 'rails_content'

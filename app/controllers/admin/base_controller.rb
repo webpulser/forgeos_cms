6a3002ac4ae3d55ec855b83bdec819c6d242d638
cveneziani <cveneziani@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-07-31 12:45:52 +0000

    skip get_menu and get_404_page for admin controllers

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,7 @@
 class Admin::BaseController < ApplicationController 
-private 
+  skip_before_filter :get_404_page, :get_menu
+
+private
   def has_flash_error? 
     not flash.blank? || flash[:error] && flash[:error].empty? 
   end 

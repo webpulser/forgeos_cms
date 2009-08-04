d1d25b2ba21100a0ae66fa99230090d9fb16c645
fcogez <fcogez@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-08-04 12:00:55 +0000

    Adding model Wnew & more

--- app/controllers/admin/widgets_controller.rb
+++ app/controllers/admin/widgets_controller.rb
@@ -1,5 +1,5 @@
 class Admin::WidgetsController < Admin::BaseController
   def index
-    @widgets = [['carousels', admin_widget_carousels_path], ['wnews.others', admin_widget_wnews_index_path]]
+    @widgets = [['carousels', admin_widget_carousels_path], ['wnews.other', admin_widget_wnews_index_path]]
   end
 end
---
+++ app/models/wnew.rb
@@ -0,0 +1,6 @@
+class Wnew < ActiveRecord::Base
+  validates_presence_of     :title
+  validates_uniqueness_of   :title
+
+  has_many  :news, :class_name => 'new'
+end

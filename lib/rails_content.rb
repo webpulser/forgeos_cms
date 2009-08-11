a2b44cc6cf394ca0ff020e93e4aa31b2fb981cb7
clepagnot <clepagnot@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-08-11 15:29:46 +0000

    - fix admin menu for new design

--- lib/rails_content.rb
+++ lib/rails_content.rb
@@ -7,18 +7,9 @@ module RailsContent
 end
 
 # Set administration's menu
-Forgeos::AdminMenu << { :title => 'sections', :url => { :controller => 'admin/sections' }, :i18n => true }
-Forgeos::AdminMenu << { :title => 'news', :url => { :controller => 'admin/news' }, :i18n => true }
-Forgeos::AdminMenu << { :title => 'pages', :url => { :controller => 'admin/pages' }, :i18n => true,
-  :children => [
-    { :title => 'blocks', :url => { :controller => 'admin/blocks' }, :i18n => true }
-  ]
-}
-Forgeos::AdminMenu << { :title => 'widgets', :url => { :controller => 'admin/widgets' }, :i18n => true,
-  :children => [
-    { :title => 'carousels', :url => { :controller => 'admin/widget_carousels' }, :i18n => true },
-    { :title => 'wnews', :url => { :controller => 'admin/widget_wnews' }, :i18n => true }
-  ]
+Forgeos::AdminMenu << { :title => 'pages',
+  :url => { :controller => 'admin/pages' }, :i18n => true,
+  :html => { :class => 'left last'}
 }
 
 # Set attachable media types

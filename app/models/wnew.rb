60aa1f84806e2b56b491610e568245fa4c184edf
fcogez <fcogez@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-08-14 13:07:35 +0000

    Fixing error dependencies

--- app/models/wnew.rb
+++ app/models/wnew.rb
@@ -1,5 +1,5 @@
 class Wnew < Widget
-  has_many  :actualities,
+  has_many  :actualities
 
   def get_actualities
     self.news_since.nil? ? self.actualities : Actuality.all(:conditions => ['created_at >= ?', self.news_since])

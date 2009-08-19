098fa4ddcf307ae329f8b6256f7778f179838c20
cveneziani <cveneziani@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-08-19 14:12:20 +0000

    bug fix: pages dataTable sorting used to not work

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -126,7 +126,7 @@ private
   end
 
   def sort
-    columns = %w(title url section_id title active)
+    columns = %w(title title url sections.title count(blocks.id) active)
     conditions = []
     per_page = params[:iDisplayLength].to_i
     offset =  params[:iDisplayStart].to_i
@@ -134,14 +134,17 @@ private
     order = "#{columns[params[:iSortCol_0].to_i]} #{params[:iSortDir_0].upcase}"
     if params[:sSearch] && !params[:sSearch].blank?
       @pages = Page.search(params[:sSearch],
+        :include => ['section', 'blocks'],
+        :group => 'pages.id',
         :order => order,
         :page => page,
         :per_page => per_page)
     else
-      @pages = Page.paginate(:all,
-        :conditions => conditions,
+      @pages = Page.paginate(:all, 
+        :include => ['section', 'blocks'],
+        :group => 'pages.id',
         :order => order,
-        :page => page,
+        :page => page, 
         :per_page => per_page)
     end
   end

a2d64bd630814aafb5de3def5014043bf2dbc03b
cveneziani <cveneziani@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-08-26 09:00:07 +0000

    updated tiny_mce conf to set toolbar position

--- app/controllers/admin/static_content_blocks_controller.rb
+++ app/controllers/admin/static_content_blocks_controller.rb
@@ -1,8 +1,10 @@
 class Admin::StaticContentBlocksController < Admin::BaseController
-
   uses_tiny_mce :only => [:new, :create, :edit, :update], :options => {
     :theme => 'advanced',
     :skin => 'forgeos',
+    :theme_advanced_toolbar_location => 'top',
+    :theme_advanced_toolbar_align => 'left',
+    :theme_advanced_statusbar_location => 'bottom',
     :theme_advanced_resizing => true,
     :theme_advanced_resize_horizontal => false,
     :plugins => %w{ table fullscreen },

9741eed1977935e29de38e3d9ddb2fbfffaffd46
fcogez <fcogez@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-10-02 13:33:26 +0000

    updating rails content init

--- init.rb
+++ init.rb
@@ -1,2 +1,2 @@
-I18n.load_path += Dir[Rails.root.join('vendor', 'plugins', 'rails_content', 'config', 'locales', '*.{rb,yml}')]
+I18n.load_path += Dir[File.join(RAILS_ROOT,'vendor', 'plugins', 'rails_content', 'config', 'locales','**', '*.{rb,yml}')]
 require 'rails_content'

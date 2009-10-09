a9c601d32adfc12127a120d2f32b20287ed2481f
clepagnot <clepagnot@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-10-09 08:47:43 +0000

    - fix menu current path detection

--- lib/rails_content.rb
+++ lib/rails_content.rb
@@ -1,6 +1,11 @@
 # Set administration's menu
 Forgeos::AdminMenu << { :title => 'pages',
-  :url => { :controller => 'admin/pages' }, :i18n => true,
+  :url => [ 
+    { :controller => 'admin/pages' },
+    { :controller => 'admin/static_content_blocks' },
+    { :controller => 'admin/widgets' }
+  ],
+  :i18n => true,
   :html => { :class => 'left last'}
 }
 

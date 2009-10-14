c3958243ccdaf576dc29b30947dfc9ee317d5184
clepagnot <clepagnot@76cce9d6-6a66-47a3-9862-acf214916d6d>
2009-10-14 13:35:22 +0000

    add global variable @sections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ private
   end
 
   def get_menu
-    sections = Section.find :all, :conditions => { :parent_id => nil, :menu => true }, :order => 'position'
-    sections.each do |section|
+    @sections = Section.find :all, :conditions => { :parent_id => nil, :menu => true }, :order => 'position'
+    @sections.each do |section|
       menu_url = section.url_for_menu
       Forgeos::Menu.insert section.position-1, menu_url unless Forgeos::Menu.include?(menu_url)
     end

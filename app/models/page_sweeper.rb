2853735dd133a09c874aab37c83287789acfe95a
cyrill62 <cyril.lepagnot@webpulser.com>
2010-03-19 09:14:53 +0100

    fix page sweeping

--- app/models/page_sweeper.rb
+++ app/models/page_sweeper.rb
@@ -1,5 +1,5 @@
 class PageSweeper < ActionController::Caching::Sweeper
-  observe Page, Block
+  observe Page, Block, Menu
 
   def after_save(record)
     expire_cache_for(record)
@@ -15,12 +15,34 @@ class PageSweeper < ActionController::Caching::Sweeper
 
   private
   def expire_cache_for(record)
-    if record.kind_of?(Page)
-      expire_page page_path(record.url)
-    else
-      record.pages.each do |page|
-        expire_page page_path(page.url)
+    case record
+    when Page
+      expire_cache_for_page(record)
+    when Menu
+      expire_all_pages
+    when Block
+      if record.single_key
+        expire_all_pages
+      else
+        record.pages.each do |page|
+          expire_cache_for_page(page)
+        end
       end
+    else
+      true
+    end
+  end
+
+  def expire_cache_for_page(page)
+    expire_page page_path(page.url)
+    page.menu_links.each do |link|
+      expire_page link.url_and_parent_urls.join
+    end
+  end
+
+  def expire_all_pages
+    Page.all.each do |page|
+      expire_cache_for_page(page)
     end
   end
 end

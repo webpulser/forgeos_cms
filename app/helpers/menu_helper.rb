e99abb820af4a5af07cd0e9b2634540943fbe159
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2010-03-31 12:22:52 +0200

    update menu_helper

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,43 +1,4 @@
 module MenuHelper
-=begin 
-  def display_menu(menu)
-    @menu.each do |section|
-      li_class = []
-      li_class << 'first ' if @menu.first.eql?(section)
-      li_class << 'last ' if @menu.last.eql?(section)
-
-      unless params[:sections].nil? or params[:sections].blank? 
-        li_class << 'active' if params[:sections].first == section.url
-      else
-        li_class << 'active' if (params[:url] && params[:url] == section.url) or params[:controller] == section.url
-      end
-      
-      conte '<li class="' + li_class.join(' ') + '">'
-      out += link_to_section section
-
-      unless section.children.nil? or section.children.blank?
-        out += '<ul>'
-        section.children.each do |sub_section|
-          if sub_section.menu
-            sub_class = ''
-            if params[:sections] && params[:sections].size > 1
-              sub_class = 'active' if params[:sections][1] == sub_section.url
-            else
-              sub_class = 'active' if params[:url] == sub_section.url
-            end
-            
-            out += '<li class="' + sub_class + '">'
-            out += link_to_section sub_section
-            out += '</li>'
-          end
-        end
-        out += '</ul>'
-      end
-      out += '</li>'
-    end
-  end
-=end
-
   def display_menu_front(menu, options = {}, &block)
     unless menu.nil?
       lis = get_menu_li(menu.menu_links, options, &block)
@@ -50,9 +11,9 @@ private
   def get_li_class(menu_link, options)
     if options[:li_current_class] && 
         # request uri equals menu_link url or one of its children url
-        (menu_link.url_and_children_urls.include?(request.request_uri) || 
+        (menu_link.url_match?(request.request_uri) || 
          # request page equals page link target or one of its children page link targe
-         (options[:page] && menu_link.is_a?(PageLink) && menu_link.page_and_children_pages.include?(options[:page])))
+         (options[:page] && menu_link.is_a?(PageLink) && menu_link.url_match?(options[:page])))
       options[:li_current_class]
     else
       options[:li_class]
@@ -61,19 +22,23 @@ private
 
   def get_menu_li(menu_links, options, &block)
     lis = []
-    menu_links.find_all_by_active(true).each do |menu_link|
+    links = menu_links.find_all_by_active(true)
+    links.each_with_index do |menu_link,index|
       if menu_link.active?
-        li_class = get_li_class menu_link, options
+        li_class = get_li_class(menu_link, options)
+        li_class = li_class ? [li_class] : [] 
+        li_class << 'first' if index == 0
+        li_class << 'last' if index == links.size - 1
         li_link = if block_given?
           capture(menu_link,&block)
         else
           link_to(menu_link.title,menu_link.url)
         end
-        children = menu_link.children.find_all_by_active(true)
+        children = menu_link.children
         unless children.empty?
-           li_link += content_tag :ul, get_menu_li(children, options).join , :class => options[:ul_class]
+           li_link += content_tag :ul, get_menu_li(children, options, &block).join , :class => options[:ul_class]
         end
-        lis << content_tag(:li, li_link, :class => li_class )
+        lis << content_tag(:li, li_link, :class => (li_class.empty? ? nil : li_class.join(' ')) )
       end
     end
     return lis

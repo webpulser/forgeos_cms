697eafbdb31df1b4269a8733604ac6fd52cb2d86
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2010-04-08 16:56:13 +0200

    add page to page_col migration possibility

--- app/models/page_col.rb
+++ app/models/page_col.rb
@@ -2,6 +2,4 @@ class PageCol < ActiveRecord::Base
   translates :content
   has_and_belongs_to_many :blocks, :list => true, :order => 'position'
   belongs_to :page
-
-  accepts_nested_attributes_for :blocks
 end

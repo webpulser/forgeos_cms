a185fe8b12ec294209d3881cadcafd1b29631c16
jclefrancois <jclefrancois@MacBook-de-jclefrancois.local>
2010-04-13 11:09:15 +0200

    fix page_links with nil pages

--- app/models/page_link.rb
+++ app/models/page_link.rb
@@ -1,8 +1,13 @@
 class PageLink < MenuLink
   def url
-    page = Page.find(self.target_id)
-    "/#{page.url}"
-  end
+    #TODO update syntax with target method
+    page = Page.find_by_id(self.target_id)
+    if page
+     "/#{page.url}"
+    else
+      "#"
+    end
+ end
 
   def page_and_children_pages
     pages = [self.target]

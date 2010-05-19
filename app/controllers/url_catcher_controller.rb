7d718aed06ff623a66ac453c5ff3e2517bbe5ad3
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2010-05-19 14:10:15 +0200

    fix #3437

--- app/controllers/url_catcher_controller.rb
+++ app/controllers/url_catcher_controller.rb
@@ -9,7 +9,11 @@ class UrlCatcherController < ApplicationController
     end
     @blocks = @page.blocks
     @page.page_viewed_counters.new.increment_counter
-    render(:action => 'show')
+    if @page.single_key == '404'
+      render(:action => 'show', :layout => true, :status => 404)
+    else
+      render(:action => 'show')
+    end
   end
 
   def root
@@ -21,27 +25,4 @@ class UrlCatcherController < ApplicationController
   def get_page
     @page = Page.find_by_url(params[:url].last, :conditions => { :active => true })
   end
-
-=begin
-  # if section exists, find page that belongs to the given section 
-  # either, find page that does not belong to a section 
-  def get_page_conditions(section)
-    {:section_id => section}
-  end
-
-  def get_page
-    # find page by url and section params
-    @section = Menu.first.menu_links.find_by_id(params[:sections])
-    @page = Page.find_by_url_and_active params[:url], true, :conditions => get_page_conditions(@section)
-    # if page is not found, find section by url param and then page
-    unless 
-      unless @section
-        @section = Menu.first.menu_links.find_by_url params[:url]
-      else
-        @section = @section.children.find_by_url params[:url]
-      end
-      @page = Page.find_by_url_and_active params[:url], true, :conditions => get_page_conditions(@section)
-    end
-  end
-=end
 end

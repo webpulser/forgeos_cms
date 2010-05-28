3742710465dd52de2dfe2ea2f211acb945446451
Sébastien Dumont <sebastien.dumont@webpulser.com>
2010-05-28 12:24:59 +0200

    page category: add page relationship

--- app/models/page_category.rb
+++ app/models/page_category.rb
@@ -1,3 +1,4 @@
 class PageCategory < Category
   has_and_belongs_to_many :elements, :join_table => 'categories_elements', :foreign_key => 'category_id', :association_foreign_key => 'element_id', :class_name => 'Page'
+  has_and_belongs_to_many :pages, :join_table => 'categories_elements', :foreign_key => 'category_id', :association_foreign_key => 'element_id'
 end

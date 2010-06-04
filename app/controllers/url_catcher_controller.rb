31a4ffc2acc04b22ca1b7941b9aa047d9436359f
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2010-06-04 14:48:18 +0200

    don't redirect for 404

--- app/controllers/url_catcher_controller.rb
+++ app/controllers/url_catcher_controller.rb
@@ -4,16 +4,12 @@ class UrlCatcherController < ApplicationController
 
   def page
     unless @page
-      page_404 = Page.find_by_single_key '404' 
-      return redirect_to(page_path(page_404.url))
+      @page = Page.find_by_single_key '404' 
+      return render(:action => 'show', :layout => true, :status => 404)
     end
     @blocks = @page.blocks
     @page.page_viewed_counters.new.increment_counter
-    if @page.single_key == '404'
-      render(:action => 'show', :layout => true, :status => 404)
-    else
-      render(:action => 'show')
-    end
+    render(:action => 'show')
   end
 
   def root

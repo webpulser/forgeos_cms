d0ef451ee105d94597d83f0ac3bd5a24ea714f2b
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-02-08 14:26:13 +0100

    refs #7547

--- public/javascripts/admin/inits/forgeos_cms/widget_faq.js
+++ public/javascripts/admin/inits/forgeos_cms/widget_faq.js
@@ -7,6 +7,7 @@ jQuery(document).ready(function(){
     var item = list.find('li:last');
     item.find('textarea').addClass('mceEditor');
     tmce_load_children('#'+item.attr('id'));
+    $('html,body').animate({scrollTop: $('#faq_'+new_id).offset().top},'slow');
     return false;
   });
 });

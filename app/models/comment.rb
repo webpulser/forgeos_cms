6f945984a9617c2f70fea262ec7389d6587902f9
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-08-04 11:53:09 +0200

    fix comment belongs_to declaration

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
 
-  belongs_to :actuality, :person
+  belongs_to :actuality
+  belongs_to :person
   belongs_to :author, :foreign_key => 'person_id', :class_name => 'Person'
 end

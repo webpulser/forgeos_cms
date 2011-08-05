25d7cd8a97e3db08d493d30294634c71a68f2a31
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-08-05 12:54:56 +0200

    update to 1.9.4 version

--- forgeos_cms_install.rb
+++ forgeos_cms_install.rb
@@ -2,10 +2,34 @@ git :init
 plugin 'attachment_fu', :git => 'git://github.com/technoweenie/attachment_fu.git'
 plugin 'open_flash_chart', :git => 'git://github.com/pullmonkey/open_flash_chart.git'
 
+gem 'rmagick'
+gem 'globalize3', '>= 0.1.0'
+gem 'acts-as-taggable-on', '>= 2.0.6'
+gem 'acts_as_list', '>= 0.1.3'
+gem 'acts_as_tree', '>= 0.1.1'
+gem 'bcrypt-ruby', '>= 2.1.4'
+gem 'fastercsv', '>= 1.5.4'
+gem 'haml', '>= 3.1.2'
+gem 'sass', '>= 3.1.4'
+gem 'mime-types', '>= 1.16'
+gem 'thinking-sphinx', '>= 2.0.5'
+gem 'webpulser-habtm_list', '>= 0.1.2'
+gem 'will_paginate', '~> 3.0.pre4'
+gem 'authlogic', '>= 3.0.3'
+gem 'forgeos_core', '>= 1.9.4'
+gem 'acts_as_commentable', '>= 3.0.1'
+gem 'forgeos_cms', '>= 1.9.4'
+
+run "bundle install"
+
 route "mount Forgeos::Cms::Engine => '/', :as => 'forgeos_cms'"
 route "mount Forgeos::Core::Engine => '/', :as => 'forgeos_core'"
 
 rake 'db:create'
+rake 'forgeos_core_engine:install:migrations'
+rake 'forgeos_cms_engine:install:migrations'
+rake 'db:migrate'
+rake 'forgeos:core:install'
 rake 'forgeos:cms:install'
 
 run 'rm -rf config/locales/* public/*.html'
